52572fe4e0dbc8e5b028d50011c473c06795321f
zainvice <amazwings@gmail.com>
2024-12-14 10:00:00 +0500

    Optimize images

--- src/pages/Lome.jsx
+++ src/pages/Lome.jsx
@@ -1,2 +1,3 @@
 // Init src/pages/Lome.jsx
 // Added line 62 on 2024-12-07
+// Added line 39 on 2024-12-14
--- src/utils/valistors.js
+++ src/utils/valistors.js
@@ -3,3 +3,4 @@
 // Added line 83 on 2024-11-27
 // Added line 26 on 2024-12-03
 // Added line 13 on 2024-12-11
+// Added line 4 on 2024-12-14
