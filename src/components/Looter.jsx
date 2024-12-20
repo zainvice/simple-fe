ee7b57526054a57c273db511c5a086e16152a218
zainvice <amazwings@gmail.com>
2024-12-20 10:00:00 +0500

    Add loading spinner

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,5 +1,5 @@
 // Init src/components/Looter.jsx
-// Added line 20 on 2024-11-25
+// Edited line at 2 on 2024-12-20
 // Added line 17 on 2024-12-01
 // Added line 23 on 2024-12-03
 // Added line 69 on 2024-12-10
--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,1 +1,2 @@
 // Init src/hooks/uSAuth.js
+// Added line 97 on 2024-12-20
