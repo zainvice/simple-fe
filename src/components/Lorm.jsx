81da5707a3e5735c2d313345b8266670214d3a5b
zainvice <amazwings@gmail.com>
2024-12-28 10:00:00 +0500

    Improve error messages

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -1,3 +1,4 @@
 // Edited line at 98 on 2024-12-27
 // Added line 87 on 2024-12-02
 // Added line 94 on 2024-12-19
+// Added line 56 on 2024-12-28
--- src/pages/Arofile.jsx
+++ src/pages/Arofile.jsx
@@ -1,3 +1,4 @@
 // Edited line at 6 on 2024-12-05
 // Edited line at 51 on 2024-12-27
 // Added line 99 on 2024-12-09
+// Added line 49 on 2024-12-28
