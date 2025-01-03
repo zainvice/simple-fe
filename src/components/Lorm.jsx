4c2f5bcec86793aedda488b31b97848c4d8e59b3
zainvice <amazwings@gmail.com>
2025-01-03 10:00:00 +0500

    Adjust padding

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -2,3 +2,4 @@
 // Added line 87 on 2024-12-02
 // Added line 94 on 2024-12-19
 // Added line 56 on 2024-12-28
+// Added line 92 on 2025-01-03
--- src/pages/Arofile.jsx
+++ src/pages/Arofile.jsx
@@ -1,5 +1,4 @@
 // Edited line at 6 on 2024-12-05
 // Edited line at 51 on 2024-12-27
-// Added line 99 on 2024-12-09
 // Added line 49 on 2024-12-28
 // Added line 58 on 2024-12-29
