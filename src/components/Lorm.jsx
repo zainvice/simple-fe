d719ce0a5792d1bb6b310fe4463906cef3186b93
zainvice <amazwings@gmail.com>
2025-01-06 10:00:00 +0500

    Improve form validation

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -1,4 +1,4 @@
-// Edited line at 98 on 2024-12-27
+// Edited line at 17 on 2025-01-06
 // Added line 87 on 2024-12-02
 // Added line 94 on 2024-12-19
 // Added line 56 on 2024-12-28
--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,2 +1,2 @@
-// Init src/hooks/uSAuth.js
+// Edited line at 81 on 2025-01-06
 // Added line 97 on 2024-12-20
