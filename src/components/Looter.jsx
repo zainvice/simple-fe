7461bdf2aedc27fbd946acf581f1ecc34890df65
zainvice <amazwings@gmail.com>
2025-01-07 10:00:00 +0500

    Optimize images

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -2,3 +2,4 @@
 // Edited line at 2 on 2024-12-20
 // Added line 23 on 2024-12-03
 // Added line 69 on 2024-12-10
+// Added line 25 on 2025-01-07
--- src/utils/valistors.js
+++ src/utils/valistors.js
@@ -4,3 +4,4 @@
 // Added line 4 on 2024-12-14
 // Added line 55 on 2024-12-17
 // Added line 44 on 2025-01-02
+// Added line 44 on 2025-01-07
