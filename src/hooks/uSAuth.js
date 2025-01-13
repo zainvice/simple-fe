c924ce4c1f41790260d8a7c66b1561735849a32d
zainvice <amazwings@gmail.com>
2025-01-13 10:00:00 +0500

    Clean up code

--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,2 +1,2 @@
-// Edited line at 81 on 2025-01-06
+// Edited line at 96 on 2025-01-13
 // Added line 97 on 2024-12-20
--- src/utils/valistors.js
+++ src/utils/valistors.js
@@ -1,5 +1,5 @@
 // Init src/utils/valistors.js
-// Added line 83 on 2024-11-27
+// Edited line at 99 on 2025-01-13
 // Added line 4 on 2024-12-14
 // Added line 55 on 2024-12-17
 // Added line 44 on 2025-01-02
