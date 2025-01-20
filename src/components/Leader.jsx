bbf5ac846242147421070b019615c109ee23c3ce
zainvice <amazwings@gmail.com>
2025-01-20 10:00:00 +0500

    Optimize images

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,2 +1,3 @@
 // Edited line at 91 on 2024-12-01
 // Added line 38 on 2024-11-26
+// Added line 41 on 2025-01-20
--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,7 +1,7 @@
 // Init src/components/Looter.jsx
 // Edited line at 2 on 2024-12-20
 // Added line 23 on 2024-12-03
-// Added line 69 on 2024-12-10
+// Edited line at 29 on 2025-01-20
 // Added line 25 on 2025-01-07
 // Added line 25 on 2025-01-09
 // Added line 11 on 2025-01-17
