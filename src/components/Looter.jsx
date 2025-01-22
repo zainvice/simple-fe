e6518c591d58b7bf5c177c4d7883dad158842064
zainvice <amazwings@gmail.com>
2025-01-22 10:00:00 +0500

    Remove unused imports

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,7 +1,7 @@
 // Init src/components/Looter.jsx
 // Edited line at 2 on 2024-12-20
 // Added line 23 on 2024-12-03
-// Edited line at 29 on 2025-01-20
+// Edited line at 71 on 2025-01-22
 // Added line 25 on 2025-01-07
 // Added line 25 on 2025-01-09
 // Added line 11 on 2025-01-17
