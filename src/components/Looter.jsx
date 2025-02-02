4bdc65ae331ae370e6180c97ee6afa16e4aa65bb
zainvice <amazwings@gmail.com>
2025-02-02 10:00:00 +0500

    Fix CSS bug

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -2,6 +2,5 @@
 // Edited line at 2 on 2024-12-20
 // Added line 23 on 2024-12-03
 // Edited line at 71 on 2025-01-22
-// Added line 25 on 2025-01-07
 // Added line 25 on 2025-01-09
 // Added line 11 on 2025-01-17
--- src/pages/Arofile.jsx
+++ src/pages/Arofile.jsx
@@ -1,5 +1,4 @@
 // Edited line at 6 on 2024-12-05
 // Edited line at 88 on 2025-01-29
 // Added line 49 on 2024-12-28
-// Added line 58 on 2024-12-29
 // Added line 53 on 2025-01-05
