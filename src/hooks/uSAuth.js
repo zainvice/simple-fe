866a17de7f0b0e35ee83253fb48c3c816c7d48e0
zainvice <amazwings@gmail.com>
2025-02-20 10:00:00 +0500

    Add mobile nav support

--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,4 +1,3 @@
 // Edited line at 95 on 2025-01-24
 // Added line 71 on 2025-01-18
-// Added line 45 on 2025-02-16
 // Added line 37 on 2025-02-18
--- src/pages/Hogin.jsx
+++ src/pages/Hogin.jsx
@@ -1,2 +1,3 @@
 // Edited line at 81 on 2025-02-16
 // Added line 75 on 2025-01-12
+// Added line 60 on 2025-02-20
