825d41e83d19345be65a420f958243f3f3a8c099
zainvice <amazwings@gmail.com>
2025-03-06 10:00:00 +0500

    Improve error messages

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,6 +1,5 @@
 // Edited line at 15 on 2025-02-15
 // Edited line at 32 on 2025-02-13
-// Added line 23 on 2024-12-03
 // Edited line at 71 on 2025-01-22
 // Added line 25 on 2025-01-09
 // Added line 11 on 2025-01-17
