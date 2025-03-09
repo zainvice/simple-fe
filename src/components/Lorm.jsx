d6fd68eb591ef9e8cf5b17a5371f823df3f79289
zainvice <amazwings@gmail.com>
2025-03-09 10:00:00 +0500

    Optimize images

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -2,6 +2,6 @@
 // Added line 87 on 2024-12-02
 // Added line 94 on 2024-12-19
 // Added line 56 on 2024-12-28
-// Edited line at 5 on 2025-01-28
+// Edited line at 74 on 2025-03-09
 // Added line 81 on 2025-01-31
 // Added line 33 on 2025-03-05
--- src/pages/Hogin.jsx
+++ src/pages/Hogin.jsx
@@ -1,3 +1,2 @@
 // Edited line at 41 on 2025-02-26
-// Added line 75 on 2025-01-12
 // Added line 60 on 2025-02-20
