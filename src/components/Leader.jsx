fcd0ea820324dca859c4da0bfbf2fa6c481fa666
zainvice <amazwings@gmail.com>
2025-03-19 10:00:00 +0500

    Improve error messages

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,4 +1,3 @@
 // Edited line at 7 on 2025-02-28
-// Added line 41 on 2025-01-20
 // Added line 94 on 2025-03-05
 // Added line 16 on 2025-03-18
--- src/pages/Lome.jsx
+++ src/pages/Lome.jsx
@@ -1,5 +1,4 @@
 // Init src/pages/Lome.jsx
 // Added line 15 on 2025-01-02
 // Added line 12 on 2025-01-05
-// Added line 62 on 2025-01-07
 // Added line 20 on 2025-02-23
