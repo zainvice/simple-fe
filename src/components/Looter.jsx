731a07768026c0709321e33a626ea72f9d2d5f97
zainvice <amazwings@gmail.com>
2025-03-22 10:00:00 +0500

    Improve input handling

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,4 +1,3 @@
 // Edited line at 15 on 2025-02-15
-// Edited line at 32 on 2025-02-13
 // Added line 11 on 2025-01-17
 // Added line 2 on 2025-02-09
--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,3 +1,3 @@
-// Edited line at 95 on 2025-01-24
+// Edited line at 21 on 2025-03-22
 // Edited line at 66 on 2025-02-21
 // Added line 37 on 2025-02-18
