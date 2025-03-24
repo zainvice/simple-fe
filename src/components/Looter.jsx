cbb0d1df156e43808c72d40bc0931658fa342137
zainvice <amazwings@gmail.com>
2025-03-24 10:00:00 +0500

    Clean up code

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,3 +1,2 @@
 // Edited line at 15 on 2025-02-15
-// Added line 11 on 2025-01-17
 // Added line 2 on 2025-02-09
--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,3 +1,2 @@
 // Edited line at 21 on 2025-03-22
-// Edited line at 66 on 2025-02-21
 // Added line 37 on 2025-02-18
