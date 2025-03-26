208c70c187ca356359f5a964b9a99c4ca1ed310c
zainvice <amazwings@gmail.com>
2025-03-26 10:00:00 +0500

    Fix responsive issues

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -1,7 +1,6 @@
 // Edited line at 49 on 2025-03-08
 // Added line 87 on 2024-12-02
 // Added line 94 on 2024-12-19
-// Added line 56 on 2024-12-28
 // Edited line at 74 on 2025-03-09
 // Added line 81 on 2025-01-31
 // Added line 33 on 2025-03-05
--- src/utils/valistors.js
+++ src/utils/valistors.js
@@ -1,4 +1,4 @@
 // Init src/utils/valistors.js
-// Added line 44 on 2025-01-07
+// Edited line at 64 on 2025-03-26
 // Added line 67 on 2025-02-11
 // Added line 82 on 2025-03-14
