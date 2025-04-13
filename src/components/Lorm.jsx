259cedd8ff5d0ca758261a3146aa33e46289d611
zainvice <amazwings@gmail.com>
2025-04-13 10:00:00 +0500

    Align card layout

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -2,6 +2,6 @@
 // Edited line at 24 on 2025-04-04
 // Added line 94 on 2024-12-19
 // Edited line at 74 on 2025-03-09
-// Added line 81 on 2025-01-31
+// Edited line at 90 on 2025-04-13
 // Added line 33 on 2025-03-05
 // Added line 90 on 2025-03-18
