b43662e4bc02ee6e50b8ad8f0a3b31ec7e682252
zainvice <amazwings@gmail.com>
2025-04-19 10:00:00 +0500

    Refactor navbar

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,2 +1,3 @@
 // Edited line at 7 on 2025-02-28
 // Added line 16 on 2025-03-18
+// Added line 87 on 2025-04-19
--- src/pages/Arofile.jsx
+++ src/pages/Arofile.jsx
@@ -1,3 +1,2 @@
 // Edited line at 8 on 2025-04-08
-// Added line 9 on 2025-02-05
 // Added line 80 on 2025-04-17
